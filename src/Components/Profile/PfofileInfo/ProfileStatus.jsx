f4058e9ba4cd8361b24ead9373e35a4366770974
Mistok <Mistreated@ukr.net>
2020-03-11 00:20:12 +0200

    profile status field autofocus added

--- src/Components/Profile/PfofileInfo/ProfileStatus.jsx
+++ src/Components/Profile/PfofileInfo/ProfileStatus.jsx
@@ -6,20 +6,32 @@ import Preloader from "../../common/preloader/preloader";
 class ProfileStatus extends React.Component{
 
     state = {
-        editMode: false
+        editMode: false,
+        title: 'yo'
     };
+    activateEditMode () {
+        this.setState({
+            editMode: true
+        });
+       // this.forceUpdate()
+    };
+    deActivateEditMode () {
+        this.setState({
+            editMode: false
+        });
 
+    };
     render(){
         return (
             <>
                 {!this.state.editMode &&
                 <div>
-                    <span>{this.props.stat}</span>
+                    <span onDoubleClick={() => {this.activateEditMode()} }>{this.props.stat}</span>
                 </div>
                 }
                 {this.state.editMode &&
                 <div>
-                    <input>{this.props.status2}</input>
+                    <input autoFocus={true} onBlur={this.deActivateEditMode.bind(this)} value={this.props.stat}/>
                 </div>
                 }
             </>
