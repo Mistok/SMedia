26c8650d347f11a2e1c3e025d5fe7039ff9c7f93
Mistok <Mistreated@ukr.net>
2020-04-06 18:59:21 +0300

    formControls refactored

--- src/Components/common/FormControls/FormControls.js
+++ src/Components/common/FormControls/FormControls.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import Class from './formControl.module.css'
 
-export const Textarea = ({input, meta, ...props}) => {
+const FormControl = ({input, meta, child, ...props}) => {
 
     let hasError = meta.error && meta.touched;
     return(
         <div className={Class.form_control + ' ' + ( hasError ? Class.error : '')}>
-            <textarea  {...input} {...props} />
+
+            {props.children}
             { hasError && <span>{meta.error}</span> }
         </div>
     )
 };
 
-export const Input = ({input, meta, ...props}) => {
-
-    let hasError = meta.error && meta.touched;
+export const Textarea = (props) =>{
+    const {input, meta, child, element, ...restProps} = props;
     return(
-        <div className={Class.form_control + ' ' + ( hasError ? Class.error : '')}>
-            <input  {...input} {...props} />
-            { hasError && <span>{meta.error}</span> }
-        </div>
+        <FormControl {...props} element={'textarea'}>
+            <textarea  {...input} {...restProps} />
+        </FormControl>
     )
 };
+
+export const Input = (props) =>{
+    const {input, meta, child, element, ...restProps} = props;
+    return(
+        <FormControl {...props} element={'input'}>
+            <input  {...input} {...restProps} />
+        </FormControl>
+    )
+};
+// export const Textarea = ({input, meta, ...props}) => {
+//
+//     let hasError = meta.error && meta.touched;
+//     return(
+//         <div className={Class.form_control + ' ' + ( hasError ? Class.error : '')}>
+//             <textarea  {...input} {...props} />
+//             { hasError && <span>{meta.error}</span> }
+//         </div>
+//     )
+// };
+
+// export const Input = ({input, meta, ...props}) => {
+//
+//     let hasError = meta.error && meta.touched;
+//     return(
+//         <div className={Class.form_control + ' ' + ( hasError ? Class.error : '')}>
+//             <input  {...input} {...props} />
+//             { hasError && <span>{meta.error}</span> }
+//         </div>
+//     )
+// };
