094000feff206b712b694b672d8f48a5a2aa43cb
Mistok <Mistreated@ukr.net>
2020-04-07 19:14:55 +0300

    logout function added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,9 @@ const Header = (props) => {
     <header className={Class.header}>
         <img src={logo} alt="logo" className={Class.header_img}/>
         <div className={Class.login_block}>
-            {props.isAuth ? <div>{props.login}</div> : <NavLink to={'/login'} activeClassName={`${Class.active}`}>Login</NavLink>}
+            {props.isAuth
+                ? <div>{props.login} <button onClick={props.logout}>Log out</button></div>
+                : <NavLink to={'/login'} activeClassName={`${Class.active}`}>Login</NavLink>}
 
         </div>
      </header>
