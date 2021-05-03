a08eba073d65b70323b4cc129071032bebe45136
mistok <Mistreated@ukr.net>
2021-05-03 23:46:19 +0300

    profile status component more tests added

--- src/Components/Profile/PfofileInfo/ProfileStatus.test.jsx
+++ src/Components/Profile/PfofileInfo/ProfileStatus.test.jsx
@@ -17,9 +17,23 @@ describe("ProfileStatus component ", () => {
     test("after creation <input> should`nt be displayed ", () => {
         const component = create(<ProfileStatus status="it-kamasutra.com"/> );
         const root = component.root;
-
         expect(() => {
             let input = root.findByType("input");
         }).toThrow();
+    });
+    test("<input> should be displayed in edit mode insteard of span", () => {
+        const component = create(<ProfileStatus status="it-kamasutra.com"/> );
+        const root = component.root;
+        let span = root.findByType("span");
+        span.props.onDoubleClick();
+        let input = root.findByType("input");
+        expect(input.props.value ).toBe("it-kamasutra.com");
+    });
+    test("callback should be called ", () => {
+        const mockCallback = jest.fn();
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatus={mockCallback}/> );
+        const instance = component.getInstance();
+        instance.deActivateEditMode();
+        expect(mockCallback.mock.calls.length).toBe(1);
     });
 });
