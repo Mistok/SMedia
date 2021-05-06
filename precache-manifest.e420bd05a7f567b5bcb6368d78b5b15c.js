ed91eb65220e60540f8bc8d60c0344c295c4b619
mistok <Mistreated@ukr.net>
2021-05-07 01:37:58 +0300

    Updates

---
+++ precache-manifest.e420bd05a7f567b5bcb6368d78b5b15c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa18c73a7836a66eda36900e583fd18f",
+    "url": "/SMedia/index.html"
+  },
+  {
+    "revision": "023f16417b340a45db40",
+    "url": "/SMedia/static/css/3.cc6e43ff.chunk.css"
+  },
+  {
+    "revision": "48ab724aa7bc0e294913",
+    "url": "/SMedia/static/css/4.389b5107.chunk.css"
+  },
+  {
+    "revision": "eeeaa57d4cd2d8cb3e7e",
+    "url": "/SMedia/static/css/main.63795b3c.chunk.css"
+  },
+  {
+    "revision": "3a22343a90f63706cc74",
+    "url": "/SMedia/static/js/2.a053313b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/SMedia/static/js/2.a053313b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023f16417b340a45db40",
+    "url": "/SMedia/static/js/3.d66c03dc.chunk.js"
+  },
+  {
+    "revision": "48ab724aa7bc0e294913",
+    "url": "/SMedia/static/js/4.6bb0a481.chunk.js"
+  },
+  {
+    "revision": "eeeaa57d4cd2d8cb3e7e",
+    "url": "/SMedia/static/js/main.34b7e0f1.chunk.js"
+  },
+  {
+    "revision": "deccb96ab1838cf7eb5e",
+    "url": "/SMedia/static/js/runtime-main.94492f32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SMedia/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aeb10bda00333851629c9d055d8cdbcb",
+    "url": "/SMedia/static/media/preloader.aeb10bda.svg"
+  }
+]);
