fedf2a10107ed70d73336c0b3393bc52ed0721ba
mistok <Mistreated@ukr.net>
2021-05-10 23:54:49 +0300

    small bugs fixed

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -127,7 +127,6 @@ export const saveProfile = (profile) => async (dispatch) => {
     let response = await profileAPI.saveProfile(profile)
     debugger
     if (response.data.resultCode === 0 ){
-        dispatch(savePhotoSuccess(response.data.data.photos));
     }
 };
 export default profileReducer;
